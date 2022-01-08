3662f435f08d1c1a49c6adf59b923721b2dfff3d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-08 10:03:18 -0300

    profile.html -> finished | profile_list.html -> finished

--- Summary/conclusion.js
+++ Summary/conclusion.js
@@ -7,3 +7,15 @@ alert('Conclusão\n\n' +
     'Você também ganhou prática adicional na configuração de um projeto Django e nas tarefas associadas a isso. Você já praticou' +
     'a leitura de mensagens de erro do Django e encontrou soluções com base nas informações que elas fornecem.\n' +
     'Você pode baixar o código para a primeira parte deste projeto clicando no link abaixo e indo para a pasta source_code_final/:')
+
+alert('Conclusao \n' +
+    'Parabéns! Neste ponto, você concluiu a segunda parte desta série de tutoriais sobre a construção de uma rede social básica com Django. \n' +
+    'Na segunda parte desta série de tutoriais, você aprendeu como: \n' +
+    '   ° Integre Bulma CSS para estilizar seu aplicativo \n' +
+    '   ° Use a herança do modelo para reduzir a repetição \n' +
+    '   ° Estruturar templates Django em uma hierarquia de pastas \n' +
+    '   ° Crie funções de roteamento e visualização \n' +
+    '   ° Interligar páginas de seu aplicativo usando URLs dinâmicos \n' +
+    'Você aprendeu como lidar com as diferentes etapas que você precisa seguir para cada página que deseja exibir usando Django, incluindo como configurar o roteamento, visualizações e modelos para ela. \n' +
+    'Você também aprendeu como aplicar o estilo CSS de uma estrutura CSS em seu aplicativo Django para melhorar a experiência do usuário com uma interface que parece agradável. \n' +
+    'Você pode baixar o código que você deve ter ao final desta parte do seu projeto clicando no link abaixo e indo para a pasta source_code_final/: ')
